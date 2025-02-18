5726db2b86eb55e42eb5ba2b19f71b92b113ebd5
Naufal <naufalpinasthika@gmail.com>
2025-02-18 13:38:48 +0700

    Initial Publish (Main Menu)
    Added MainMenuScene

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuCo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public CanvasGroup OptionPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Option()
+    {
+        OptionPanel.alpha = 1;
+        OptionPanel.blocksRaycasts = true;
+    }
+
+    public void Back()
+    {
+        OptionPanel.alpha = 0;
+        OptionPanel.blocksRaycasts = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
