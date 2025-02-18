d89647a4a5d98a2ec289270d2768aa8e7840c600
Raysha Erviandika Putra <19624013@mahasiswa.itb.ac.id>
2025-02-18 09:27:43 +0700

    Addition : Window Drag and Drop, Window Spawning

---
+++ Assets/Scripts/WindowSpawn.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WindowSpawn : MonoBehaviour
+{
+    public GameObject[] window;
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
+
+    private Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();    
+        StartCoroutine(spawnWindow());
+    }
+    IEnumerator spawnWindow()
+    {
+        while (true)
+        {
+            GameObject win = Instantiate(window[0]);
+            Vector2 pos = new Vector2(Random.Range(minBounds.x, maxBounds.x), Random.Range(minBounds.y, maxBounds.y));
+            win.transform.position = clampBorder(pos);
+            yield return new WaitForSeconds(1);
+        }
+    }
+    Vector2 clampBorder(Vector2 pos)
+    {
+        pos.x = Mathf.Clamp(pos.x, minBounds.x, maxBounds.x);
+        pos.y = Mathf.Clamp(pos.y, minBounds.y, maxBounds.y);
+        return pos;
+    }
+
+    public void closeButton()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        anim.SetTrigger("close");
+    }
+}
