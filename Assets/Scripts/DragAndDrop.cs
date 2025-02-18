c84770e13502d344d4a4e4b6f9481a4016cd20cb
Raysha Erviandika Putra <19624013@mahasiswa.itb.ac.id>
2025-02-18 08:54:07 +0700

    Initial commit

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Vector2 offset;
+    public Vector2 minBounds;
+    public Vector2 maxBounds; 
+    void HandleDragging()
+    {
+        Vector2 newPosition = mousepos() + offset;       
+        transform.position = clampBorder(newPosition);
+    }
+    void OnMouseDown()
+    {
+        offset = (Vector2)transform.position - mousepos();
+    }
+    private void OnMouseDrag()
+    {
+        HandleDragging();
+    }
+    Vector2 mousepos()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    Vector2 clampBorder(Vector2 pos)
+    {
+        pos.x = Mathf.Clamp(pos.x, minBounds.x, maxBounds.x);
+        pos.y = Mathf.Clamp(pos.y, minBounds.y, maxBounds.y);
+        return pos;
+    }
+}
